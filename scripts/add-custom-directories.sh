2a9844c9841b834e67b197f11babc0059063c0cc
Johan Yngman <johan.yngman@gmail.com>
2024-01-28 12:02:31 +0100

    Tweak custom directory logic.

--- scripts/add-custom-directories.sh
+++ scripts/add-custom-directories.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-CUSTOM_FILE="$USERDATA_DIR/custom"
+CUSTOM_DIRECTORIES_FILE="$USERDATA_DIR/custom-directories"
 
 echo "Verifying and adding custom directory list..."
 
-if [ ! -f "$CUSTOM_FILE" ]; then
-    echo "Creating '$CUSTOM_FILE' with default directory '~'"
-    echo "~" > "$CUSTOM_FILE"
+if [ ! -f "$CUSTOM_DIRECTORIES_FILE" ]; then
+    echo "Creating '$CUSTOM_DIRECTORIES_FILE' with default directory '~'"
+    echo "~" > "$CUSTOM_DIRECTORIES_FILE"
 fi
 
-file_contents=$(cat "$CUSTOM_FILE")
+file_contents=$(cat "$CUSTOM_DIRECTORIES_FILE")
 eval_and_verify_directories "$file_contents"
 
 if [[ -n "$file_contents" ]]; then
