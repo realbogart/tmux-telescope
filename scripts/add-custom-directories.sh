40d7e8269c7b7727094eba94c1345276036684f7
Johan Yngman <johan.yngman@gmail.com>
2024-01-28 11:56:00 +0100

    Add logic and documentation for custom directories for Directory Sessions Picker.

--- scripts/add-custom-directories.sh
+++ scripts/add-custom-directories.sh
@@ -4,11 +4,15 @@ CUSTOM_FILE="$USERDATA_DIR/custom"
 
 echo "Verifying and adding custom directory list..."
 
-if [ -f "$CUSTOM_FILE" ]; then
-    file_contents=$(cat "$CUSTOM_FILE")
-    eval_and_verify_directories "$file_contents"
+if [ ! -f "$CUSTOM_FILE" ]; then
+    echo "Creating '$CUSTOM_FILE' with default directory '~'"
+    echo "~" > "$CUSTOM_FILE"
+fi
+
+file_contents=$(cat "$CUSTOM_FILE")
+eval_and_verify_directories "$file_contents"
+
+if [[ -n "$file_contents" ]]; then
     echo -e "$verified_dirs" > "$DIRECTORY_SESSIONS_FILE"
-else
-    echo "'$CUSTOM_FILE' not found. Skipping."
 fi
 
